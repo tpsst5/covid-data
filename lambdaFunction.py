87ccf689dbb00e6a23628b18567b41c8a92ca613
Timothy Shea <tshea31@gmail.com>
2021-01-07 20:59:42 -0500

    Add AWS lambda functionality

--- lambdaFunction.py
+++ lambdaFunction.py
@@ -2,28 +2,34 @@
 
 import csv
 import urllib.request
+import json
 
-# Data from NYT
-nytData = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
-nytResponse = urllib.request.urlopen(nytData)
-nytContents = csv.reader(nytResponse.read().decode('utf-8').splitlines())
+def lambda_handler(event, context):
+    # Data from NYT
+    nyt_data = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
+    nyt_response = urllib.request.urlopen(nyt_data)
+    nyt_contents = csv.reader(nyt_response.read().decode('utf-8').splitlines())
 
-# Data from JHU for recoveries
-jhuData = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv?opt_id=oeu1606875624763r0.5077944167011417'
-jhuResponse = urllib.request.urlopen(jhuData)
-jhuContents = csv.reader(jhuResponse.read().decode('utf-8').splitlines())
+    # Data from JHU for recoveries
+    jhu_data = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv?opt_id=oeu1606875624763r0.5077944167011417'
+    jhu_response = urllib.request.urlopen(jhu_data)
+    jhu_contents = csv.reader(jhu_response.read().decode('utf-8').splitlines())
 
-# Dictionary to store the data
-dataDict = {}
+    # Dictionary to store the data
+    data_dict = {}
 
-# Add NYT data to dictionary
-for row in nytContents:
-    if row[0] != 'date':
-        dataDict[row[0]] = {'cases': row[1], 'deaths': row[2], 'recovered': 0}
+    # Add NYT data to dictionary
+    for row in nyt_contents:
+        if row[0] != 'date':
+            data_dict[row[0]] = {'cases': row[1], 'deaths': row[2], 'recovered': 0}
 
-# Update dictionary to include recoveries from JHU data
-for row in jhuContents:
-    if row[1] == 'US':
-        dataDict[row[0]]['recovered'] = row[4]
+    # Update dictionary to include recoveries from JHU data
+    for row in jhu_contents:
+        if row[1] == 'US':
+            data_dict[row[0]]['recovered'] = row[4]
 
-# print("2020-12-10 data: ", dataDict['2020-12-10'])
+    print("2020-12-10 data: ", data_dict['2020-12-10'])
+    return {
+        'statusCode': 200,
+        'body': data_dict
+    }
